b95c23d61ab96496534f9a1e6d36ad473e020597
Zvonimir <zvonimirplivelic91@gmail.com>
2022-06-02 19:00:15 +0200

    Successfully displaying data

--- app/src/main/java/com/zvonimirplivelic/randimal/ui/AnimalInfoFragment.kt
+++ app/src/main/java/com/zvonimirplivelic/randimal/ui/AnimalInfoFragment.kt
@@ -5,10 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.ProgressBar
+import android.widget.TextView
 import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.squareup.picasso.Picasso
 import com.zvonimirplivelic.randimal.R
 import com.zvonimirplivelic.randimal.RandimalViewModel
 import com.zvonimirplivelic.randimal.util.Resource
@@ -18,19 +22,34 @@ class AnimalInfoFragment : Fragment() {
 
     private lateinit var viewModel: RandimalViewModel
 
-    private lateinit var progressBar: ProgressBar
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_animal_info, container, false)
 
-        progressBar = view.findViewById(R.id.progress_bar)
+        val progressBar: ProgressBar = view.findViewById(R.id.progress_bar)
+        val fabRefresh: FloatingActionButton = view.findViewById(R.id.fab_get_animal_info)
+
+        val ivImage: ImageView = view.findViewById(R.id.iv_image)
+        val tvName: TextView = view.findViewById(R.id.tv_name)
+        val tvLatinName: TextView = view.findViewById(R.id.tv_latin_name)
+        val tvType: TextView = view.findViewById(R.id.tv_type)
+        val tvActiveTime: TextView = view.findViewById(R.id.tv_active_time)
+        val tvLength: TextView = view.findViewById(R.id.tv_length)
+        val tvWeight: TextView = view.findViewById(R.id.tv_weight)
+        val tvLifespan: TextView = view.findViewById(R.id.tv_lifespan)
+        val tvHabitat: TextView = view.findViewById(R.id.tv_habitat)
+        val tvGeoRange: TextView = view.findViewById(R.id.tv_geo_range)
+        val tvDiet: TextView = view.findViewById(R.id.tv_diet)
 
         viewModel = ViewModelProvider(this)[RandimalViewModel::class.java]
 
         viewModel.getAnimalInformation()
+
+        fabRefresh.setOnClickListener {
+            viewModel.getAnimalInformation()
+        }
 
         viewModel.randomAnimalInfo.observe(viewLifecycleOwner) { response ->
             when (response) {
@@ -38,8 +57,17 @@ class AnimalInfoFragment : Fragment() {
                     progressBar.isVisible = false
                     response.data?.let { animalInfoResponse ->
 
-                        Timber.d("AnimalInfoResponse $animalInfoResponse")
-
+                        Picasso.get().load(animalInfoResponse.imageLink).into(ivImage)
+                        tvName.text = animalInfoResponse.name
+                        tvLatinName.text = animalInfoResponse.latinName
+                        tvType.text = animalInfoResponse.animalType
+                        tvActiveTime.text = animalInfoResponse.activeTime
+                        tvLength.text = animalInfoResponse.lengthMax
+                        tvWeight.text = animalInfoResponse.weightMax
+                        tvLifespan.text = animalInfoResponse.lifespan
+                        tvHabitat.text = animalInfoResponse.habitat
+                        tvGeoRange.text = animalInfoResponse.geoRange
+                        tvDiet.text = animalInfoResponse.diet
                     }
                 }
 
